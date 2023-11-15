268558d91248986b31ae1553277e2b2bc8ec9a43
jagv091205 <jagveerkaurkhalsa@gmail.com>
2023-11-15 19:22:19 +0530

    All files

---
+++ task-minder/src/components/Note.js
@@ -0,0 +1,20 @@
+import { MdDeleteForever } from 'react-icons/md';
+import React from 'react';
+
+const Note = ({ id, text, date, handleDeleteNote }) => {
+	return (
+		<div className='note'>
+			<span>{text}</span>
+			<div className='note-footer'>
+				<small>{date}</small>
+				<MdDeleteForever
+					onClick={() => handleDeleteNote(id)}
+					className='delete-icon'
+					size='1.3em'
+				/>
+			</div>
+		</div>
+	);
+};
+
+export default Note;
